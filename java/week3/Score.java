1d9ff3c921a7d7713763103abc4ac9a593200829
dPfla <choyr2016@naver.com>
2021-01-06 21:24:40 +0900

    [ADD] 자바공부 1일차

---
+++ java/week3/Score.java
@@ -0,0 +1,28 @@
+package week3;
+
+public class Score {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 8;
+		
+		if(a >= 9)
+		{
+			System.out.println("1");
+		}
+		else if (a>=7)
+		{
+			System.out.println("2");
+		}
+		else if (a >=5)
+		{
+			System.out.println("3");
+		}
+		else
+		{
+			System.out.println("else");
+		}
+	}
+
+}
